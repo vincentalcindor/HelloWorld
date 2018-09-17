d1a674537441ce8f5f6d23a47b16276245460c14
Vince <vincent.alcindor@gmail.com>
2018-09-17 21:17:27 +0100

    Add windows Java code

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.vince.tutorials;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("Hello world from intellij");
+    }
+}
+
