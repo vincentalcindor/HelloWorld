a49437148d6cc68a621e28c51163523806e4c67c
Vince <vincent.alcindor@gmail.com>
2018-09-18 09:08:55 +0100

    check in changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("Hello world from intellij");
+        System.out.println("Testing source control Check in");
     }
 }
 
